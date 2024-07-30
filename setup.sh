c4f8491fbfe7946a53cef8f785784ce9c4763c49
sliman4 <4sliman4@gmail.com>
2024-07-31 02:58:48 +0300

    Add setup.sh for private module shims

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+SUBMODULE_PATH="./xeon-private-modules"
+
+declare -a SHIM_PACKAGES=("aqua" "ft-buybot" "new-liquidity-pools" "nft-buybot" "potlock" "price-alerts" "socialdb" "new-tokens" "honey")
+
+if [ -f "$SUBMODULE_PATH/.git" ] || [ -d "$SUBMODULE_PATH/.git" ]; then
+    echo "Submodule $SUBMODULE_PATH already exists."
+    exit 0
+fi
+
+git submodule update --init --recursive $SUBMODULE_PATH
+
+if [ -f "$SUBMODULE_PATH/.git" ]; then
+    echo "Private modules successfully downloaded."
+else
+    echo "Failed to download private submodule. Using only open-source modules..."
+
+    mkdir -p $SUBMODULE_PATH
+
+    cd $SUBMODULE_PATH || exit
+
+    for package in "${SHIM_PACKAGES[@]}"; do
+        cargo new "$package"
+    done
+
+    git init
+
+    echo "Shim crates installed in $SUBMODULE_PATH, the project is ready to be built using 'cargo build'. You may have seen some warnings like 'failed to load manifest for workspace member $PWD/tearbot', 'failed to load manifest for dependency', 'failed to read $PWD/xeon-private-modules/something/Cargo.toml', you can safely ignore them."
+fi
